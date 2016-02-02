497f4316e910b2cefe91db6378e18a6f0086961f
Florian Waibel <fwaibel@eclipsesource.com>
2016-02-02 11:50:16 +0100

    Updates spring web socket lower bounds of imports for tomcat to 7.0.47 and jetty to 9.1.

--- recipes/springframework/install-springframework-4.2.1.RELEASE.sh
+++ recipes/springframework/install-springframework-4.2.1.RELEASE.sh
@@ -1,7 +1,7 @@
 export GROUP_ID="org.eclipse.virgo.mirrored"
 export VERSION="4.2.1.RELEASE"
 
-for i in aop aspects beans context context.support core expression jdbc jms messaging orm oxm test tx web webmvc webmvc.portlet websocket
+for i in aop aspects beans context context.support core expression jdbc jms messaging orm oxm test transaction web webmvc webmvc.portlet websocket
 do
 mvn install:install-file -Dfile=org.springframework.${i}_${VERSION}/target/org.springframework.${i}-${VERSION}.jar -DgroupId=${GROUP_ID} -DartifactId=org.springframework.${i} -Dversion=${VERSION} -Dpackaging=jar
 done
